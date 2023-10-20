b8f307c4b309c942edc0955af34c3e7e317b3efc
Afaz Siddiqui <aafazsiddiqui@gmail.com>
2023-10-20 20:50:54 +0500

    Add files via upload

---
+++ test.php
@@ -0,0 +1,36 @@
+<?php
+
+
+$a = 2;
+// $b;
+$b = 8;
+
+$c = $a + $b;
+
+// $a =+ $b;
+// $a += $b;
+// $a =++ $b;
+
+// echo $a;
+// echo $b;
+
+
+// if($a = $b){
+//     echo $a + $b;
+// }
+
+// else{
+//     echo "Error";
+// }
+
+if($b = $a){
+    echo $a + $b;
+}
+
+else{
+    echo "Error";
+}
+
+
+
+?>
